6c91186e9d1945128b9d61269b4d3e7e3d20e2a1
Logan Hufstetler <logan.hufstetler@gmail.com>
2019-02-27 15:28:08 -0700

    set the friendsList to state.

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,9 +1,28 @@
 import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
 
 import DisplayFList from './components/DisplayFList.js';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      friendsList: []
+    };
+  }
+
+  componentDidMount() {
+    axios
+    .get('http://localhost:5000/friends')
+    .then(res => {
+      this.setState({ friendsList: res.data });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+  }
+
   render() {
     return (
       <div>

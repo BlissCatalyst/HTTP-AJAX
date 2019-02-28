20f5cd825526abf6ffe9f08facfd735e0e3bd060
Logan Hufstetler <logan.hufstetler@gmail.com>
2019-02-27 17:06:50 -0700

    added a form to get the info for a new friend.

--- friends/src/components/DisplayFList.js
+++ friends/src/components/DisplayFList.js
@@ -2,15 +2,72 @@ import React from 'react'
 
 import FriendCard from './FriendCard';
 
-const DisplayFList = props => {
+class DisplayFList extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: '',
+            age: [],
+            email: ''
+        };
+    }
 
+    handleChanges = e => {
+        this.setState({ [e.target.name]: e.target.value });
+    };
+
+    addName = (e, name, age, email) => {
+        e.preventDefault();
+        const newFriend = {
+            name: name,
+            age: age,
+            email: email
+        }
+    }
+
+    submitFriend = e => {
+        this.setState({ 
+            name: '',
+            age: [],
+            email: ''
+         })
+         this.addName(e, this.state.item, this.state.age, this.state.email)
+    };
+
+    render() {
     return(
-      <section>
-          {props.friendsList.map((friend, index) => (
-              <FriendCard friend={friend} key={index} />
-          ))}
-      </section>
+        <div>
+            <section>
+                {this.props.friendsList.map((friend, index) => (
+                    <FriendCard friend={friend} key={index} />
+                ))}
+            </section>
+            <section>
+                <form onSubmit={this.submitFriend}>
+                    <input 
+                        type="text"
+                        value={this.state.name}
+                        name="name"
+                        onChange={this.handleChanges}
+                    />
+                    <input 
+                        type="number"
+                        value={this.state.age}
+                        name="age"
+                        onChange={this.handleChanges}
+                    />
+                    <input 
+                        type="text"
+                        value={this.state.email}
+                        name="email"
+                        onChange={this.handleChanges}
+                    />
+                </form>
+            </section>
+        </div>
     );
+
   }
+}
 
 export default DisplayFList;
